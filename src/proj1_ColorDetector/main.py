87469fd04a100c2bcd1e8afe48a4f04f2715720b
Hafiz Hanif <nhafiz13.work@gmail.com>
2025-09-29 02:23:04 +0200

    1st commit project 2

--- src/proj1_ColorDetector/main.py
+++ src/proj1_ColorDetector/main.py
@@ -1,23 +1,36 @@
 import cv2
+from PIL import Image
+
 from utils import get_limits
+import sys
+print(sys.executable)
 
-cap= cv2.VideoCapture(1)
 
-yellow = [0,255, 255] # yellow in BGR
+yellow = [0, 255, 255]  # yellow in BGR colorspace
+cap = cv2.VideoCapture(1)
 while True:
     ret, frame = cap.read()
 
-    hsvImage = cv2.cvtcolor(frame, cv2.COLOR_BGR2HSV)
+    hsvImage = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-    lower_limit, upper_limit = get_limits(color=yellow)
-    
-    mask = cv2.inRange(hsvImage, lower_limit, upper_limit)
+    lowerLimit, upperLimit = get_limits(color=yellow)
+
+    mask = cv2.inRange(hsvImage, lowerLimit, upperLimit)
+
+    mask_ = Image.fromarray(mask)
+
+    bbox = mask_.getbbox()
+
+    if bbox is not None:
+        x1, y1, x2, y2 = bbox
+
+        frame = cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 5)
+
     cv2.imshow('frame', frame)
-    
-    if cv2.waitKey(40) & 0xFF == ord('q'):
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
-    cv2.destroyAllWindows()
+cap.release()
 
-    
-
+cv2.destroyAllWindows()
